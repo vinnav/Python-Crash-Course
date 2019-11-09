bce79a6b328fe23eb48d84b899d29946f1bf2354
vinnav <54999963+vinnav@users.noreply.github.com>
2019-11-09 14:32:44 +0000

    create 7-2 and 7-3 exercises

---
+++ 7InputAndWhile/7-2.RestaurantSeating.py
@@ -0,0 +1,6 @@
+people = input("How many people are in your group? Insert number: ")
+people = int(people)
+if people > 8:
+    print("I'm afraid you will have to wait for an available table...")
+else:
+    print("Come this way, your table is ready!")
